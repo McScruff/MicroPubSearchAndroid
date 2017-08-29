0733acf5da53771bc06952edafb942eb5484b6f6
Unknown <mcscruff@gmail.com>
2017-08-29 03:06:18 +0100

    Second activity added

---
+++ app/src/main/java/com/tts/mcscruff/micropubsearchandroid/activity_popup.kt
@@ -0,0 +1,12 @@
+package com.tts.mcscruff.micropubsearchandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_popup : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_popup)
+    }
+}
